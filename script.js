06a48a68d2bbffd22c61cb0993cb650c58e51981
Siddwap <58450503+Siddwap@users.noreply.github.com>
2022-08-27 23:43:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Change the second argument to your options:
 // https://github.com/sampotts/plyr/#options
-const player = new Plyr('video', { captions: { active: true }, ads: { enabled: true, tagUrl: '#' } });
+const player = new Plyr('video', { captions: { active: true }, ads: { enabled: true, tagUrl: 'https://syndication.exdynsrv.com/splash.php?idzone=4774980' } });
 
 // Expose player so it can be used from the console
 window.player = player;
